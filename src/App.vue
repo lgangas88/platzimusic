07f8af400eeb0d5d4cb4f8e6cfceb62bad152121
luis <luis.agv88@gmail.com>
2019-04-11 13:43:40 -0500

    [ADD] first commit

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+
+<template lang='pug'>
+  #app
+    img(src='./assets/logo.png')
+    h1 PlatziMusic
+    select(v-model='countrySelected')
+      option(v-for='country in countries' :value='country.value') {{country.name}}
+    spinner(v-show='showSpinner')
+    ul
+      artist(v-for='artist in artists' :artist='artist' :key='artist.mbid')
+</template>
+
+<script>
+import getArtists from "./api";
+import artist from "./components/Artist.vue";
+import spinner from './components/Spinner.vue'
+
+export default {
+  name: "app",
+  data() {
+    return {
+      artists: [],
+      countries: [
+        { name: "Argentina", value: "argentina" },
+        { name: "Peru", value: "peru" },
+        { name: "Chile", value: "chile" }
+      ],
+      countrySelected : 'argentina',
+      showSpinner : false
+    };
+  },
+  components: {
+    artist,
+    spinner
+  },
+  methods : {
+    refreshArtist : function () {
+      this.showSpinner = true
+      this.artists = []
+      getArtists(this.countrySelected).then(artists => {
+        this.artists = artists
+        this.showSpinner = false
+      })
+    }
+  },
+  mounted: function() {
+    this.refreshArtist()
+  },
+  watch: {
+    countrySelected :function () {
+      this.refreshArtist()
+    }
+  }
+};
+</script>
+
+<style lang='stylus'>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+  appname : 'platzimusic',
+  apiKey : 'a2e3616752b8a0166a9bad4d5994b3f4',
+  secret : '13564aa1aad4e45e13e2c9d8ab33f276',
+  registeredTo : 'lgangas'
+}
